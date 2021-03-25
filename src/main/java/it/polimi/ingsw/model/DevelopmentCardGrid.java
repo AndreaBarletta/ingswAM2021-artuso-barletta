dd28b240eed089f54caefa993ab4dc71ec272541
Luca-Arpaia <79997946+Luca-Arpaia@users.noreply.github.com>
2021-03-25 19:25:49 +0100

    Fixed Market and started DevelopmentCardGrid.

--- src/main/java/it/polimi/ingsw/model/DevelopmentCardGrid.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCardGrid.java
@@ -1,4 +1,16 @@
 package it.polimi.ingsw.model;
 
+import java.util.Stack;
+
 public class DevelopmentCardGrid {
+
+    private Stack<DevelopmentCard>[][] cardGrid;
+
+    public DevelopmentCardGrid(Stack<DevelopmentCard>[][] cardGrid) {
+        this.cardGrid = new Stack<DevelopmentCard>[4][3];
+    }
+
+    public Stack<DevelopmentCard>[][] getCardGrid() {
+        return cardGrid;
+    }
 }
