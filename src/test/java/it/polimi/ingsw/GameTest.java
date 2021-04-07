09c1c697b5f50f1d108c7d478a2176f47769128f
Andrea Barletta <10665046@polimi.it>
2021-04-07 23:28:29 +0200

    Test for loading cards from file

--- src/test/java/it/polimi/ingsw/GameTest.java
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -1,4 +1,21 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.Game;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class GameTest {
+    @Test
+    public void testLoadFromFile(){
+        Game game=new Game();
+        try {
+            game.loadDevelopmentCardsFromFile("src/main/java/it/polimi/ingsw/resources/developmentCards.json");
+        }catch(IOException e){
+            System.out.println("Error while loading development cards from file");
+        }
+
+        assertEquals(game.getDevelopmentCardGrid().size(),48);
+    }
 }
