dad58df135c1d2b489744431635743e107024c82
Andrea Barletta <10665046@polimi.it>
2021-04-07 19:32:02 +0200

    removeCard now returns the card removed, useful for when players pick a card

--- src/main/java/it/polimi/ingsw/model/DevelopmentCardGrid.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCardGrid.java
@@ -5,9 +5,9 @@ public class DevelopmentCardGrid {
 
     public DevelopmentCardGrid() {
         cardGrid = new DevelopmentCardGridCell[3][4];
-        for(DevelopmentCardGridCell[] row:cardGrid){
-            for(DevelopmentCardGridCell cell:row){
-                cell=new DevelopmentCardGridCell();
+        for (int row = 0; row < 3; row ++){
+            for (int col = 0; col < 4; col++){
+                cardGrid[row][col]=new DevelopmentCardGridCell();
             }
         }
     }
@@ -24,10 +24,11 @@ public class DevelopmentCardGrid {
      *
      * @param level level of the card to remove
      * @param cardType card type of the card to remove
+     * @return returns the card removed
      * Remove a card from the selected pile of the grid
      */
-    public void removeCard(int level, CardType cardType){
-        cardGrid[level-1][cardType.ordinal()].removeCard();
+    public DevelopmentCard removeCard(int level, CardType cardType){
+        return cardGrid[level-1][cardType.ordinal()].removeCard();
     }
 
     /**
