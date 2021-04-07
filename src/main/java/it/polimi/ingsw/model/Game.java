31a05b56c0d6313c11b70c00b3aa8fb0480651a5
Andrea Barletta <10665046@polimi.it>
2021-04-07 23:27:32 +0200

    Added loading of the development card grid from json file

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,8 +1,27 @@
 package it.polimi.ingsw.model;
 
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class Game {
     private PersonalBoard[] personalBoards;
     private Market market;
     private DevelopmentCardGrid developmentCardGrid;
 
+    public void loadDevelopmentCardsFromFile(String path) throws IOException {
+        File file=new File(path);
+        String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
+        Gson gson=new Gson();
+        developmentCardGrid=gson.fromJson(content,DevelopmentCardGrid.class);
+    }
+
+    public DevelopmentCardGrid getDevelopmentCardGrid(){
+        return developmentCardGrid;
+    }
 }
