a43874d7d843b13add1c869ddd31230f3b7a3a8e
Andrea Barletta <10665046@polimi.it>
2021-04-08 17:42:30 +0200

    Development cards are saved in the json file as a list of cards instead of the json representation of the grid

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -3,22 +3,29 @@ package it.polimi.ingsw.model;
 import com.google.gson.Gson;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Path;
 
 public class Game {
     private PersonalBoard[] personalBoards;
     private Market market;
     private DevelopmentCardGrid developmentCardGrid;
 
+    public Game(){
+        market=new Market();
+        developmentCardGrid=new DevelopmentCardGrid();
+    }
+
     public void loadDevelopmentCardsFromFile(String path) throws IOException {
         File file=new File(path);
         String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
         Gson gson=new Gson();
-        developmentCardGrid=gson.fromJson(content,DevelopmentCardGrid.class);
+        DevelopmentCard developmentCards[]=gson.fromJson(content, DevelopmentCard[].class);
+
+        for(DevelopmentCard d:developmentCards){
+            developmentCardGrid.addCard(d);
+        }
     }
 
     public DevelopmentCardGrid getDevelopmentCardGrid(){
--- src/test/java/it/polimi/ingsw/GameTest.java
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -1,9 +1,15 @@
 package it.polimi.ingsw;
 
-import it.polimi.ingsw.model.Game;
+import com.google.gson.Gson;
+import it.polimi.ingsw.model.*;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
