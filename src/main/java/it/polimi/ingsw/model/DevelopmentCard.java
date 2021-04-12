dedf8594bf7b47fd275da9dd2e0c896aa1102aa6
Andrea Barletta <10665046@polimi.it>
2021-04-13 00:26:26 +0200

    Added javadoc comments

--- src/main/java/it/polimi/ingsw/model/DevelopmentCard.java
+++ src/main/java/it/polimi/ingsw/model/DevelopmentCard.java
@@ -3,13 +3,25 @@ package it.polimi.ingsw.model;
 import java.util.Map;
 
 public class DevelopmentCard {
+    // Level of the card
     private int level;
+    // Cost of the card
     private Map<ResType,Integer> cost;
+    //Victory points given by the card
     private int victoryPoints;
+    // Card type
     private CardType cardType;
+    // Production of the card
     private Production production;
 
-
+    /**
+     * Creates a new card with the given level, cost, victoryPoints, cardType and production
+     * @param level Level of the card
+     * @param cost Cost of the card
+     * @param victoryPoints Victory points given by the card
+     * @param cardType Card type
+     * @param production Production of the card
+     */
     public DevelopmentCard(int level, Map<ResType,Integer> cost, int victoryPoints, CardType cardType, Production production){
         this.level=level;
         this.cost=cost;
@@ -19,7 +31,7 @@ public class DevelopmentCard {
     }
 
     /**
-     *
+     * Gets the level of the card
      * @return Level of the card
      */
     public int getLevel() {
@@ -27,15 +39,15 @@ public class DevelopmentCard {
     }
 
     /**
-     *
-     * @return Resources requires to buy the development card
+     * Gets the resources requires to buy the card
+     * @return Resources requires to buy the card
      */
     public Map<ResType, Integer> getCost() {
         return cost;
     }
 
     /**
-     *
+     * Gets the victory points of the card
      * @return Victory points of the card
      */
     public int getVictoryPoints() {
@@ -43,7 +55,7 @@ public class DevelopmentCard {
     }
 
     /**
-     *
+     * Gets the type of the card
      * @return Type of the card
      */
     public CardType getCardType() {
@@ -51,7 +63,7 @@ public class DevelopmentCard {
     }
 
     /**
-     *
+     * Gets the resources produced by the production of the card
      * @return Resources produced by the development card
      */
     public Map<ResType, Integer> produce() {
@@ -59,7 +71,7 @@ public class DevelopmentCard {
     }
 
     /**
-     *
+     * Gets the resources required to activate the production of the card
      * @return Resources required by the development card to activate production
      */
     public Map<ResType, Integer> getIngredients(){
