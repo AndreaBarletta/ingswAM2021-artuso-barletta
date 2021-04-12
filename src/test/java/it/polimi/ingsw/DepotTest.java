0ac2f6860de0682e66ffc4e4e821324d86e3b430
Andrea Barletta <10665046@polimi.it>
2021-04-13 00:21:12 +0200

    Added test class

---
+++ src/test/java/it/polimi/ingsw/DepotTest.java
@@ -0,0 +1,19 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.exceptions.DepotResourceTypeException;
+import it.polimi.ingsw.model.exceptions.DepotSpaceException;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DepotTest {
+    @Test
+    public void testAdd() {
+        Depot depot = new Depot(2);
+        depot.setDepotResource(ResType.COIN);
+
+        assertDoesNotThrow(() -> depot.add(ResType.COIN, 1));
+        assertThrows(DepotResourceTypeException.class,()->depot.add(ResType.SHIELD,1));
+        assertThrows(DepotSpaceException.class,()->depot.add(ResType.COIN,20));
+    }
+}
