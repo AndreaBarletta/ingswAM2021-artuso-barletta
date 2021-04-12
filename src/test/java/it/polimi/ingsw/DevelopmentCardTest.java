30948618a70e1c2742c045321febc85ae7e8a2ae
Andrea Barletta <10665046@polimi.it>
2021-04-12 23:12:41 +0200

    Added tests for DevelopmentCard class

---
+++ src/test/java/it/polimi/ingsw/DevelopmentCardTest.java
@@ -0,0 +1,32 @@
+package it.polimi.ingsw;
+
+import it.polimi.ingsw.model.CardType;
+import it.polimi.ingsw.model.DevelopmentCard;
+import it.polimi.ingsw.model.Production;
+import it.polimi.ingsw.model.ResType;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DevelopmentCardTest {
+    @Test
+    public void testGetter(){
+        Map<ResType,Integer> cost=new HashMap<>();
+        cost.put(ResType.COIN,1);
+        cost.put(ResType.SERVANT,2);
+
+        Map<ResType,Integer> ingredients=new HashMap<>();
+        ingredients.put(ResType.STONE,1);
+        Map<ResType,Integer> products=new HashMap<>();
+        products.put(ResType.SHIELD,3);
+        products.put(ResType.FAITH,1);
+        Production production=new Production(ingredients,products);
+
+        DevelopmentCard developmentCard=new DevelopmentCard(1,cost,10, CardType.GREEN,production);
+
+        assertEquals(cost,developmentCard.getCost());
+        assertEquals(ingredients,developmentCard.getIngredients());
+    }
+}
