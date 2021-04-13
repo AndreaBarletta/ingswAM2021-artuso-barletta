70abce2e248c9b390af55364c75d3071d64a24ef
Andrea Barletta <10665046@polimi.it>
2021-04-13 17:17:47 +0200

    Added test for add and remove

--- src/test/java/it/polimi/ingsw/StrongboxTest.java
+++ src/test/java/it/polimi/ingsw/StrongboxTest.java
@@ -1,10 +1,45 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.model.ResType;
+import it.polimi.ingsw.model.Strongbox;
+import it.polimi.ingsw.model.exceptions.StrongboxNegQuantityException;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.util.HashMap;
 
 public class StrongboxTest {
     @Test
-    public void testAdd() {
+    public void testAddRemove() {
+        Strongbox strongbox=new Strongbox();
 
+        HashMap<ResType,Integer> expected=new HashMap<>();
+        expected.put(ResType.COIN,10);
+        expected.put(ResType.SHIELD,20);
+
+        strongbox.add(expected);
+
+        assertEquals(strongbox.getContent(),expected);
+
+        HashMap<ResType,Integer> toRemove=new HashMap<>();
+
+        //Correct usage
+        toRemove.put(ResType.SHIELD,20);
+        expected.put(ResType.SHIELD,0);
+        assertDoesNotThrow(()->strongbox.remove(toRemove));
+        assertEquals(strongbox.getContent(),expected);
+
+        //Removal of too much of a given resource
+        toRemove.put(ResType.COIN,20);
+        assertThrows(StrongboxNegQuantityException.class,()->strongbox.remove(toRemove));
+
+        //Check if changes reverted after failed remove
+        assertEquals(strongbox.getContent(),expected);
+
+        //Removal of a resource not present in the storage
+        toRemove.put(ResType.STONE,20);
+        assertThrows(StrongboxNegQuantityException.class,()->strongbox.remove(toRemove));
+
+        //Check if changes reverted after failed remove
+        assertEquals(strongbox.getContent(),expected);
     }
 }
