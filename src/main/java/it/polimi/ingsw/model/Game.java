4981757074c3e7c248ea4f290679635d29af888f
Andrea Barletta <10665046@polimi.it>
2021-04-14 14:57:58 +0200

    Added check for malformed json file

--- src/main/java/it/polimi/ingsw/model/Game.java
+++ src/main/java/it/polimi/ingsw/model/Game.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.model;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
 import it.polimi.ingsw.controller.ControllerEventListener;
 
 import java.io.File;
@@ -34,16 +35,19 @@ public class Game implements GameEventListener {
     /**
      * Loads developments cards from a json file and creates the development card grid, putting the cards in the right cells based on the level and card type
      * @param path Path of the json file containing the  list of development cards
-     * @throws IOException
+     * @throws IOException Error while reading file i.e. wrong path
      */
     public void loadDevelopmentCardsFromFile(String path) throws IOException {
         File file=new File(path);
         String content = new String(Files.readAllBytes(file.toPath()), StandardCharsets.UTF_8);
         Gson gson=new Gson();
-        DevelopmentCard[] developmentCards=gson.fromJson(content, DevelopmentCard[].class);
-
-        for(DevelopmentCard d:developmentCards){
-            developmentCardGrid.addCard(d);
+        try{
+            DevelopmentCard[] developmentCards=gson.fromJson(content, DevelopmentCard[].class);
+            for(DevelopmentCard d:developmentCards){
+                developmentCardGrid.addCard(d);
+            }
+        }catch(JsonSyntaxException e){
+            System.out.println("Error loading json file for development cards");
         }
     }
 
