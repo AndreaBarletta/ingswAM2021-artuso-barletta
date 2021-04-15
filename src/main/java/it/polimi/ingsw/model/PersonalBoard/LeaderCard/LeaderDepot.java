8e47e4daf77b5e10709f07b8c97bbf44170c29fb
Andrea Barletta <10665046@polimi.it>
2021-04-15 16:57:54 +0200

    Added costructor

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/LeaderCard/LeaderDepot.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/LeaderCard/LeaderDepot.java
@@ -1,12 +1,21 @@
 package it.polimi.ingsw.model.PersonalBoard.LeaderCard;
 
+import it.polimi.ingsw.model.CardType;
 import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCard;
 import it.polimi.ingsw.model.PersonalBoard.Depot;
 import it.polimi.ingsw.model.PersonalBoard.PersonalBoard;
+import it.polimi.ingsw.model.ResType;
+
+import java.util.Map;
 
 public class LeaderDepot extends LeaderCard {
     private Depot depot;
 
+    public LeaderDepot(int victoryPoints, Map<CardType, Integer> requirements, int capacity, ResType resource){
+        super(victoryPoints,requirements);
+        depot=new Depot(capacity);
+        depot.setDepotResource(resource);
+    }
     public void effectOnDraw(PersonalBoard personalBoard){}
     public void effectOnMarketBuy(PersonalBoard personalBoard){}
     public void effectOnDevCardBuy(PersonalBoard personalBoard, DevelopmentCard developmentCard){}
