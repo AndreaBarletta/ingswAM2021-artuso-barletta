8db80b66c617c40260a3995d805ec5435b215806
Luca-Arpaia <79997946+Luca-Arpaia@users.noreply.github.com>
2021-04-15 18:42:13 +0200

    Fixed testGetCards

--- src/test/java/it/polimi/ingsw/PersonalBoardTest/DevelopmentCardSlotTest.java
+++ src/test/java/it/polimi/ingsw/PersonalBoardTest/DevelopmentCardSlotTest.java
@@ -26,7 +26,7 @@ public class DevelopmentCardSlotTest {
         ingredients.put(ResType.SERVANT, 1);
         HashMap<ResType, Integer> products = new HashMap<>();
         products.put(ResType.SHIELD, 2);
-        production = new Production(products, ingredients);
+        production = new Production(ingredients, products);
 
         DevelopmentCard devCard1 = new DevelopmentCard(1, cost, 3, CardType.BLUE, production);
         DevelopmentCard devCard2 = new DevelopmentCard(2, cost, 3, CardType.BLUE, production);
@@ -48,9 +48,25 @@ public class DevelopmentCardSlotTest {
 
     public void testGetCards(){
         DevelopmentCardSlot devCardSlot = new DevelopmentCardSlot();
+
+        HashMap<ResType, Integer> cost = new HashMap<>();
+        cost.put(ResType.COIN, 1);
+        Production production;
+        HashMap<ResType, Integer> ingredients = new HashMap<>();
+        ingredients.put(ResType.SERVANT, 1);
+        HashMap<ResType, Integer> products = new HashMap<>();
+        products.put(ResType.SHIELD, 2);
+        production = new Production(products, ingredients);
+
         DevelopmentCard[] devCards = new DevelopmentCard[2];
-        for(DevelopmentCard d: devCards){
-            assertDoesNotThrow(() -> devCardSlot.addCard(d));
+
+        devCards[0] = new DevelopmentCard(1, cost, 3, CardType.BLUE, production);
+        devCards[1] = new DevelopmentCard(2, cost, 3, CardType.BLUE, production);
+
+        try{
+            devCardSlot.addCard(devCards[0]);
+            devCardSlot.addCard(devCards[1]);
+        }catch (LevelException e){
         }
 
         assertEquals(devCardSlot.getCards(), devCards);
