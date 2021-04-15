dc48db179d54ca845375c98615af0bb8c1260bc6
Andrea Barletta <10665046@polimi.it>
2021-04-15 18:01:02 +0200

    Removed unnecessary includes

--- src/main/java/it/polimi/ingsw/model/exceptions/DepotResourceTypeException.java
+++ src/main/java/it/polimi/ingsw/model/exceptions/DepotResourceTypeException.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.model.exceptions;
 
-import it.polimi.ingsw.model.exceptions.DepotException;
-
 public class DepotResourceTypeException extends DepotException {
     public DepotResourceTypeException() {
         super();
--- src/main/java/it/polimi/ingsw/model/exceptions/DepotSpaceException.java
+++ src/main/java/it/polimi/ingsw/model/exceptions/DepotSpaceException.java
@@ -1,7 +1,5 @@
 package it.polimi.ingsw.model.exceptions;
 
-import it.polimi.ingsw.model.exceptions.DepotException;
-
 public class DepotSpaceException extends DepotException {
     public DepotSpaceException(){
         super();
