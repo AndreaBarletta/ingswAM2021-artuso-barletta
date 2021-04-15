483dc2e5607b28b020ea9f839b4e87bae9305850
Andrea Barletta <10665046@polimi.it>
2021-04-15 23:22:11 +0200

    Added tests

--- src/test/java/it/polimi/ingsw/GameTest.java
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw;
 
+import it.polimi.ingsw.controller.Controller;
 import it.polimi.ingsw.model.*;
+import it.polimi.ingsw.model.PersonalBoard.PersonalBoard;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -11,7 +13,31 @@ public class GameTest {
         Game game=new Game();
         assertTrue(game.loadDevelopmentCardsFromFile("src/main/resources/developmentCards.json"));
         assertTrue(game.loadPopeFavourCardsFromFile("src/main/resources/popeFavourCards.json"));
+        assertTrue(game.loadLeaderCardsFromFile("src/main/resources/leaderCards.json"));
 
         assertEquals(game.getDevelopmentCardGrid().size(),48);
+    }
+
+    @Test
+    public void testAddPlayer(){
+        Game game=new Game();
+        Controller controller=new Controller();
+        controller.addEventListener(game);
+        game.addEventListener(controller);
+
+        game.addPlayer("player 1");
+        game.addPlayer("player 2");
+    }
+
+    @Test
+    public void testGiveLeaderCards(){
+        Game game=new Game();
+        Controller controller=new Controller();
+        PersonalBoard personalBoard=new PersonalBoard("test");
+        controller.addEventListener(game);
+        game.addEventListener(controller);
+
+        game.loadLeaderCardsFromFile("src/main/resources/leaderCards.json");
+
     }
 }
