5b4d0298519d7359d8a6f5255397f5c581d5c0ec
Matteo Artuso <10610150@polimi.it>
2021-04-15 10:37:26 +0200

    DevelopmentCardSlot class created

---
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/DevelopmentCardSlot.java
@@ -0,0 +1,18 @@
+package it.polimi.ingsw.model.PersonalBoard;
+
+import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCard;
+
+import java.util.Stack;
+
+public class DevelopmentCardSlot {
+    //attributes
+    private Stack<DevelopmentCard> devCardsSlot;
+
+    //constructor
+    public DevelopmentCardSlot(){
+
+    }
+
+    //methods
+
+}
