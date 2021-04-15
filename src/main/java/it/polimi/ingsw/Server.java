b3a023ddc60ec1de2742fb322cd6046d370e543e
Andrea Barletta <10665046@polimi.it>
2021-04-15 17:45:52 +0200

    Testing

--- src/main/java/it/polimi/ingsw/Server.java
+++ src/main/java/it/polimi/ingsw/Server.java
@@ -1,12 +1,19 @@
 package it.polimi.ingsw;
 
+import com.google.gson.Gson;
+import it.polimi.ingsw.model.CardType;
 import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.model.PersonalBoard.LeaderCard.LeaderDepot;
+import it.polimi.ingsw.model.ResType;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Server {
     public static void main(String[] args){
         Game game=new Game();
 
-        if (game.loadDevelopmentCardsFromFile("src/main/resources/developmentCards.json")) {
+        if (game.loadLeaderCardsFromFile("src/main/resources/leaderCards.json")) {
             System.out.println("OK");
         } else {
             System.out.println("NO");
