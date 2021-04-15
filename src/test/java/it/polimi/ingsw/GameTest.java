86a57ad455f6847a898aaf416f850cd110f9487c
Andrea Barletta <10665046@polimi.it>
2021-04-15 22:27:11 +0200

    Changed paths

--- src/test/java/it/polimi/ingsw/GameTest.java
+++ src/test/java/it/polimi/ingsw/GameTest.java
@@ -1,14 +1,7 @@
 package it.polimi.ingsw;
 
-import com.google.gson.Gson;
 import it.polimi.ingsw.model.*;
 import org.junit.jupiter.api.Test;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,8 +9,8 @@ public class GameTest {
     @Test
     public void testLoadFromFile(){
         Game game=new Game();
-        assertTrue(game.loadDevelopmentCardsFromFile("src/main/java/it/polimi/ingsw/resources/developmentCards.json"));
-        assertTrue(game.loadPopeFavorCardsFromFile("src/main/java/it/polimi/ingsw/resources/developmentCards.json"));
+        assertTrue(game.loadDevelopmentCardsFromFile("src/main/resources/developmentCards.json"));
+        assertTrue(game.loadPopeFavourCardsFromFile("src/main/resources/popeFavourCards.json"));
 
         assertEquals(game.getDevelopmentCardGrid().size(),48);
     }
