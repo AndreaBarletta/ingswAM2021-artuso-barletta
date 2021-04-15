8dfac05ca00ccc2c828282290f24c6d2e440d227
Matteo Artuso <10610150@polimi.it>
2021-04-15 11:13:09 +0200

    DevelopmentCardSlot attributes and methods

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/DevelopmentCardSlot.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/DevelopmentCardSlot.java
@@ -1,7 +1,9 @@
 package it.polimi.ingsw.model.PersonalBoard;
 
 import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCard;
+import it.polimi.ingsw.model.ResType;
 
+import java.util.Map;
 import java.util.Stack;
 
 public class DevelopmentCardSlot {
@@ -10,9 +12,19 @@ public class DevelopmentCardSlot {
 
     //constructor
     public DevelopmentCardSlot(){
-
+        devCardsSlot = new Stack<>();
     }
 
     //methods
+    public int getCardsInSlot() {
+        return devCardsSlot.size();     //devCardsSlot.capacity();
+    }
 
+    public void addCard(DevelopmentCard devCard) {
+        devCardsSlot.push(devCard);
+    }
+
+    public Map<ResType, Integer> getIngredients() {
+        return devCardsSlot.peek().getIngredients();
+    }
 }
