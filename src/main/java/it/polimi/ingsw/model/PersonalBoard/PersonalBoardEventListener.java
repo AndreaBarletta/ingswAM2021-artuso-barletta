94ee66d7ae0c5b21c1457f2ab6d5b75182fb95fc
Andrea Barletta <10665046@polimi.it>
2021-04-16 02:42:15 +0200

    Added events

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/PersonalBoardEventListener.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/PersonalBoardEventListener.java
@@ -1,7 +1,12 @@
 package it.polimi.ingsw.model.PersonalBoard;
 
+import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCard;
+import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCardGrid;
 import it.polimi.ingsw.model.PersonalBoard.LeaderCard.LeaderCard;
 
 public interface PersonalBoardEventListener {
     int[] chooseLeaderCards(LeaderCard[] leaderCards,String playerName);
+    DevelopmentCard chooseDevelopmentCard(DevelopmentCardGrid cardGrid, String playerName);
+    int chooseDevelopmentCardSlot(DevelopmentCardSlot[] developmentCardSlots,DevelopmentCard card, String playerName);
+    void error(String error);
 }
