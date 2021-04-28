5e772a109f298db38273f8f15d54866ca4756737
Luca-Arpaia <79997946+Luca-Arpaia@users.noreply.github.com>
2021-04-28 14:03:08 +0200

    Started FaithTrackTest

--- src/test/java/it/polimi/ingsw/PersonalBoardTest/FaithTrackTest/FaithTrackTest.java
+++ src/test/java/it/polimi/ingsw/PersonalBoardTest/FaithTrackTest/FaithTrackTest.java
@@ -6,4 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class FaithTrackTest {
 
+    @Test
+    public void testIncrementFaithTrack(){
+        FaithTrack faithTrack = new FaithTrack();
+
+        int faithPoint = 5;
+        int faithTest = 5;
+
+        //Increment faithMarker of 5
+        faithTrack.incrementFaithTrack(faithPoint);
+
+        //Check the increment starting with faithMarker = 0
+        assertEquals(faithTrack.getPosition(), faithTest);
+
+        faithPoint = 3;
+        faithTest = 8;
+
+        //Increment faithMarker of 3
+        faithTrack.incrementFaithTrack(faithPoint);
+
+        //Check the increment starting with faithMarker != 0
+        assertEquals(faithTrack.getPosition(), faithTest);
+    }
+
 }
