d6e30dbb87adbdaeeb1a21374b654423996997de
Luca-Arpaia <79997946+Luca-Arpaia@users.noreply.github.com>
2021-04-29 15:10:09 +0200

    Completed FaithTrackTest

--- src/test/java/it/polimi/ingsw/PersonalBoardTest/FaithTrackTest/FaithTrackTest.java
+++ src/test/java/it/polimi/ingsw/PersonalBoardTest/FaithTrackTest/FaithTrackTest.java
@@ -27,6 +27,19 @@ public class FaithTrackTest {
 
         //Check the increment starting with faithMarker != 0
         assertEquals(faithTrack.getPosition(), faithTest);
+
+        faithPoint = 30;
+        faithTest = 24;
+
+        //Try to increment faithMarker more then the max (max = 24)
+        faithTrack.incrementFaithTrack(faithPoint);
+
+        //Check if increment is blocked at 24 (max of faithTrack)
+        assertEquals(faithTrack.getPosition(), faithTest);
+
+        //Check isAtEnd boolean
+        assertTrue(faithTrack.isAtEnd());
+
     }
 
 }
