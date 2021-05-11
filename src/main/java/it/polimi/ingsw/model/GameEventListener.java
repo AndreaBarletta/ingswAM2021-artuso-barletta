a9d4d14e4d1671cc9134df1e8b50cfdd4ed2468a
AndreaBarletta <10665046@polimi.it>
2021-05-12 00:33:48 +0200

    Removed method

--- src/main/java/it/polimi/ingsw/model/GameEventListener.java
+++ src/main/java/it/polimi/ingsw/model/GameEventListener.java
@@ -3,7 +3,6 @@ package it.polimi.ingsw.model;
 import it.polimi.ingsw.model.PersonalBoard.PersonalBoard;
 
 public interface GameEventListener {
-    void newPersonalBoard(PersonalBoard newPersonalBoard);
     void inkwellGiven(String playerName);
     void announceWinner(String playerName);
 }
