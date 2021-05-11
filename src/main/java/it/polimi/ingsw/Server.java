771c6471dd6877d32866c8a2f7a0f580b77ce583
Koeltekn <barletta.andrea@outlook.com>
2021-05-11 16:33:38 +0200

    Added server sockets

--- src/main/java/it/polimi/ingsw/Server.java
+++ src/main/java/it/polimi/ingsw/Server.java
@@ -1,15 +1,37 @@
 package it.polimi.ingsw;
 
-import com.google.gson.Gson;
-import it.polimi.ingsw.model.CardType;
-import it.polimi.ingsw.model.Game;
-import it.polimi.ingsw.model.PersonalBoard.LeaderCard.LeaderDepot;
-import it.polimi.ingsw.model.ResType;
-
-import java.util.HashMap;
-import java.util.Map;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
+    static final int defaultPortNumber=4545;
+
     public static void main(String[] args){
+        int portNumber;
+        if(args.length==1){
+            portNumber = Integer.parseInt(args[0]);
+        }else{
+            portNumber = defaultPortNumber;
+        }
+
+        ServerSocket serverSocket;
+        try{
+            serverSocket = new ServerSocket(portNumber);
+        }catch(IllegalArgumentException e){
+            System.out.println("Invalid port number");
+            return;
+        }catch(Exception e){
+            System.out.print("Error while opening server socket");
+            return;
+        }
+        System.out.print("Hello");
+        try {
+            Socket clientSocket = serverSocket.accept();
+        }catch(Exception e){
+            System.out.println("Error while accepting client socket");
+            return;
+        }
+
+        System.out.println("Accepted client");
     }
 }
