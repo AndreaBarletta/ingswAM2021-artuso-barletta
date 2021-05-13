341fc5a912a2f3b4bbfcc60d2d3674c3264c7383
AndreaBarletta <10665046@polimi.it>
2021-05-13 18:12:20 +0200

    Removed method

--- src/main/java/it/polimi/ingsw/model/ResType.java
+++ src/main/java/it/polimi/ingsw/model/ResType.java
@@ -4,21 +4,4 @@ import it.polimi.ingsw.model.PersonalBoard.PersonalBoard;
 
 public enum ResType {
     COIN,STONE,SERVANT,SHIELD,FAITH,WHITEMARBLE,ANY;
-
-    public void effectOnAcquire(PersonalBoard personalBoard){
-        switch(this){
-            case COIN:
-                break;
-            case STONE:
-                break;
-            case SERVANT:
-                break;
-            case SHIELD:
-                break;
-            case FAITH:
-                break;
-            case WHITEMARBLE:
-                break;
-        }
-    }
 }
