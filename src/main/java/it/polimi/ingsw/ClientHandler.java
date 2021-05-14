4a0ebc0fd2576160b8db57af9e255743b5d2d541
AndreaBarletta <10665046@polimi.it>
2021-05-14 10:44:31 +0200

    Server now only accepts certain type of messages at any time, based on what the player can do

--- src/main/java/it/polimi/ingsw/ClientHandler.java
+++ src/main/java/it/polimi/ingsw/ClientHandler.java
@@ -18,6 +18,7 @@ public class ClientHandler implements Runnable{
     private Map<String, Controller> games;
     private String playerName;
     private Controller currentGame;
+    private MessageType[] expectedMessageType;
 
     public ClientHandler(Socket clientSocket,Map<String,Controller> games){
         this.clientSocket=clientSocket;
@@ -29,46 +30,61 @@ public class ClientHandler implements Runnable{
         try{
             out=new PrintWriter(clientSocket.getOutputStream(),true);
             in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            expectedMessageType=new MessageType[]{MessageType.CONNECT};
             out.println(new Message(MessageType.OK,new String[]{"true"}));
             String messageString=in.readLine();
             Gson gson=new Gson();
+            boolean accepted=false;
             while(messageString!=null){
                 System.out.println(messageString);
                 Message message=gson.fromJson(messageString,Message.class);
-                switch(message.messageType){
-                    case CONNECT:
-                        System.out.println("Player "+message.params[0]+" has connected");
-                        this.playerName=message.params[0];
-                        out.println(new Message(MessageType.OK,new String[]{"true"}));
+                for(MessageType mt:expectedMessageType){
+                    if(mt==message.messageType){
+                        System.out.println("Accepted message");
+                        accepted=true;
                         break;
-                    case CREATEGAME:
-                        if(message.params.length==2){
-                            if(games.get(message.params[0])==null){
-                                Controller newController=new Controller();
-                                newController.createGame(this,message.params[0],Integer.valueOf(message.params[1]));
-                                games.put(message.params[0],newController);
-                                currentGame=newController;
-                                out.println(new Message(MessageType.OK,new String[]{"false","Waiting for other players..."}));
-                            }else{
-                                //Game with the same name already created
-                                send(new Message(MessageType.ERROR,new String[]{"Game with the same name already exists"}));
-                            }
-                        }else{
-                            //Too few arguments
-                            send(new Message(MessageType.NOTENOUGHARGUMENTS,new String[]{}));
-                        }
-                        break;
-                    case JOINGAME:
-                        Controller game=games.get(message.params[0]);
-                        if(game!=null){
-                            out.println(new Message(MessageType.OK,new String[]{"false","Waiting for other players..."}));
-                            game.joinGame(this);
-                            currentGame=game;
-                        }else{
-                            out.println(new Message(MessageType.ERROR,new String[]{"Game does not exist"}));
-                        }
-                        break;
+                    }
                 }
+                if(accepted){
+                    switch(message.messageType){
+                        case CONNECT:
+                            System.out.println("Player "+message.params[0]+" has connected");
+                            this.playerName=message.params[0];
+                            expectedMessageType=new MessageType[]{MessageType.CREATEGAME,MessageType.JOINGAME};
+                            out.println(new Message(MessageType.OK,new String[]{"true"}));
+                            break;
+                        case CREATEGAME:
+                            if(message.params.length==2){
+                                if(games.get(message.params[0])==null){
+                                    Controller newController=new Controller();
+                                    newController.createGame(this,message.params[0],Integer.valueOf(message.params[1]));
+                                    games.put(message.params[0],newController);
+                                    currentGame=newController;
+                                    out.println(new Message(MessageType.OK,new String[]{"false","Waiting for other players..."}));
+                                }else{
+                                    //Game with the same name already created
+                                    send(new Message(MessageType.ERROR,new String[]{"Game with the same name already exists"}));
+                                }
+                            }else{
+                                //Too few arguments
+                                send(new Message(MessageType.NOTENOUGHARGUMENTS,new String[]{}));
+                            }
+                            break;
+                        case JOINGAME:
+                            Controller game=games.get(message.params[0]);
+                            if(game!=null){
+                                out.println(new Message(MessageType.OK,new String[]{"false","Waiting for other players..."}));
+                                game.joinGame(this);
+                                currentGame=game;
+                            }else{
+                                out.println(new Message(MessageType.ERROR,new String[]{"Game does not exist"}));
+                            }
+                            break;
+                    }
+                }else{
+                    send(new Message(MessageType.ERROR,new String[]{"Cannot use that command now"}));
+                }
+                accepted=false;
                 messageString=in.readLine();
             }
         }catch(Exception e){
@@ -84,5 +100,9 @@ public class ClientHandler implements Runnable{
 
     public String getPlayerName(){
         return playerName;
+    }
+
+    public void setExpectedMessageType(MessageType[] expectedMessageType){
+        this.expectedMessageType=expectedMessageType;
     }
 }
