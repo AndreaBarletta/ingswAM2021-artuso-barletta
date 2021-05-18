05211e09f1642239149dbba4700ada4f9ff04558
Matteo Artuso <10610150@polimi.it>
2021-05-18 10:43:38 +0200

    added some fsa state

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/TurnAction.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/TurnAction.java
@@ -1,5 +1,5 @@
 package it.polimi.ingsw.model.PersonalBoard;
 
 public enum TurnAction {
-    GETRESOURCES,BUYDEVCARD,ACTIVATEPRODUCTION
+    GETRESOURCES,BUYDEVCARD,ACTIVATEPRODUCTION,LEADERACTION
 }
