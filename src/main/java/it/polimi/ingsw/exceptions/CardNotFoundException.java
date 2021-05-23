f6fbe785ae136c6338474298a7d158d38ac7b9b1
AndreaBarletta <10665046@polimi.it>
2021-05-23 19:49:03 +0200

    Added exceptions for the activation of leader cards

---
+++ src/main/java/it/polimi/ingsw/exceptions/CardNotFoundException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.exceptions;
+
+public class CardNotFoundException extends Exception{
+    public CardNotFoundException(){super();}
+}
---
+++ src/main/java/it/polimi/ingsw/exceptions/CardTypeException.java
@@ -0,0 +1,5 @@
+package it.polimi.ingsw.exceptions;
+
+public class CardTypeException extends Exception{
+    public CardTypeException(){super();}
+}
