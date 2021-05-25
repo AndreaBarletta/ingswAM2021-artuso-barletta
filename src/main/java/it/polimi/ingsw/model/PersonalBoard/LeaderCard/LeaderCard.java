bc47da553ad69377ac30a64da717c1eff26eb177
AndreaBarletta <10665046@polimi.it>
2021-05-25 14:37:20 +0200

    Added method to check if a leader card can be bought given some resources and development cards

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/LeaderCard/LeaderCard.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/LeaderCard/LeaderCard.java
@@ -5,6 +5,8 @@ import it.polimi.ingsw.model.DevelopmentCard.DevelopmentCard;
 import it.polimi.ingsw.model.PersonalBoard.PersonalBoard;
 import it.polimi.ingsw.model.ResType;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public abstract class LeaderCard {
@@ -51,4 +53,52 @@ public abstract class LeaderCard {
     public abstract void effectOnDevCardBuy(PersonalBoard personalBoard, DevelopmentCard developmentCard);
     public abstract void effectOnDiscard(PersonalBoard personalBoard);
     public abstract String toString();
+
+    /**
+     * Checks if a leader card can be bought
+     * @param resources Resources a player has
+     * @param devCards Development cards a players has
+     * @return Whether or not the card can be bought
+     */
+    public boolean canActivate(Map<ResType,Integer> resources, List<DevelopmentCard> devCards) {
+        //Check resource requirements
+        if(resourceRequirements!=null){
+            for(Map.Entry<ResType,Integer> p:resourceRequirements.entrySet()){
+                if(!resources.containsKey(p.getKey()))
+                    return false;
+                else if(resources.get(p.getKey())<p.getValue())
+                    return false;
+            }
+        }
+
+        //Get number of cards per type and maximum level per type
+        Map<CardType,Integer> cardTypes=new HashMap<>();
+        Map<CardType,Integer> cardMaxLevels=new HashMap<>();
+        for(DevelopmentCard d:devCards){
+            cardTypes.compute(d.getCardType(),(k,v)->v==null?d.getLevel():v+d.getLevel());
+            cardMaxLevels.compute(d.getCardType(),(k,v)->d.getLevel());
+        }
+
+        //Check card requirements
+        if(cardRequirements!=null){
+            for(Map.Entry<CardType,Integer> p:cardRequirements.entrySet()){
+                if(!cardTypes.containsKey(p.getKey()))
+                    return false;
+                else if(cardTypes.get(p.getKey())<p.getValue())
+                    return false;
+            }
+        }
+
+        //Check level requirements
+        if(levelRequirements!=null){
+            for(Map.Entry<CardType,Integer> p: levelRequirements.entrySet()){
+                if(!cardMaxLevels.containsKey(p.getKey()))
+                    return false;
+                else if(cardMaxLevels.get(p.getKey())<p.getValue())
+                    return false;
+            }
+        }
+
+        return true;
+    }
 }
