a7a56151cd72cc11a761f1abde87816e8c975938
AndreaBarletta <10665046@polimi.it>
2021-05-26 11:19:16 +0200

    Added colors

--- src/main/java/it/polimi/ingsw/view/Colors.java
+++ src/main/java/it/polimi/ingsw/view/Colors.java
@@ -1,13 +1,25 @@
 package it.polimi.ingsw.view;
 
 public enum Colors {
-    RED("\u001B[31m"),
-    GREEN("\u001B[32m"),
-    YELLOW("\u001B[33m"),
-    BLUE("\u001B[34m");
+    BLACK("\u001b[30m"),
+    RED("\u001b[31m"),
+    GREEN("\u001b[32m"),
+    YELLOW("\u001b[33m"),
+    BLUE("\u001b[34m"),
+    MAGENTA("\u001b[35m"),
+    CYAN("\u001b[36m"),
+    WHITE("\u001b[37m"),
+    GRAY("\u001b[30;1m"),
+    BRIGHT_RED("\u001b[31;1m"),
+    BRIGHT_GREEN("\u001b[32;1m"),
+    BRIGHT_YELLOW("\u001b[33;1m"),
+    BRIGHT_BLUE("\u001b[34;1m"),
+    BRIGHT_MAGENTA("\u001b[35;1m"),
+    BRIGHT_CYAN("\u001b[36;1m"),
+    BRIGHT_WHITE("\u001b[37;1m"),
+    RESET("\u001B[0m");
 
     private String escape;
-    static final String RESET = "\u001B[0m";
 
     Colors(String escape) {
         this.escape = escape;
