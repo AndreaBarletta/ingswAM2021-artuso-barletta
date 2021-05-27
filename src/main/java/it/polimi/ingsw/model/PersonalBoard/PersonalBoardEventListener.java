7f00b7b51d7e67aeb33185ba84245df05088a6f6
Matteo Artuso <10610150@polimi.it>
2021-05-27 17:26:22 +0200

    added method

--- src/main/java/it/polimi/ingsw/model/PersonalBoard/PersonalBoardEventListener.java
+++ src/main/java/it/polimi/ingsw/model/PersonalBoard/PersonalBoardEventListener.java
@@ -1,4 +1,8 @@
 package it.polimi.ingsw.model.PersonalBoard;
 
+import it.polimi.ingsw.model.PersonalBoard.LeaderCard.LeaderCard;
+import it.polimi.ingsw.model.Production;
+
 public interface PersonalBoardEventListener {
+    public void addedLeaderProduction(String playerName);
 }
