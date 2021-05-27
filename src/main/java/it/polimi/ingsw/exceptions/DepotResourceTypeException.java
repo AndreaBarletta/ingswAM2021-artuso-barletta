7b20560355d32a79eeb3ffd6a92468202528851b
AndreaBarletta <10665046@polimi.it>
2021-05-27 12:52:03 +0200

    Removed depot exception (subclasses are now used)

--- src/main/java/it/polimi/ingsw/exceptions/DepotResourceTypeException.java
+++ src/main/java/it/polimi/ingsw/exceptions/DepotResourceTypeException.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.exceptions;
 
-public class DepotResourceTypeException extends DepotException {
+public class DepotResourceTypeException extends Exception {
     public DepotResourceTypeException() {
         super();
     }
