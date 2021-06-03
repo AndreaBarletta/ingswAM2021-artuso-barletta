5445b5badd70cbf2492e0eced4889e4bc36e79ad
AndreaBarletta <10665046@polimi.it>
2021-06-03 10:19:54 +0200

    Fixed leader depots

--- src/main/java/it/polimi/ingsw/view/LightPersonalBoard.java
+++ src/main/java/it/polimi/ingsw/view/LightPersonalBoard.java
@@ -14,19 +14,18 @@ public class LightPersonalBoard {
     private List<Integer> leaderCards;
     private FaithTrack faithTrack;
     private Stack<Integer>[] developmentCardSlots;
-    private List<Map.Entry<ResType, Integer>> depots = new ArrayList<>();
-    private List<Map.Entry<ResType, Integer>> leaderDepots = new ArrayList<>();
-    private Map<ResType, Integer> strongbox = new HashMap<>();
-    private boolean inkwell = false;
+    private List<Map.Entry<ResType, Integer>> depots;
+    private List<Map.Entry<ResType, Integer>> leaderDepots;
+    private Map<ResType, Integer> strongbox;
+    private boolean inkwell;
     private Production baseProduction;
     private List<Production> leaderProductions;
-    private List<Depot> leaderDepots;
     private List<ResType> leaderConverts;
     private List<Map<ResType,Integer>> leaderDiscounts;
 
     public LightPersonalBoard(String playerName) {
         this.playerName=playerName;
-        developmentCardSlots=new Stack[3]; //TODO check
+        //developmentCardSlots=new Stack<>[3];
         for(int i=0;i<3;i++){
             developmentCardSlots[i]=new Stack<>();
         }
@@ -40,22 +39,13 @@ public class LightPersonalBoard {
         Map<ResType,Integer> baseProducts=new HashMap<>();
         baseProducts.put(ResType.ANY,1);
         baseProduction=new Production(baseIngredients,baseProducts);
+        depots = new ArrayList<>();
+        strongbox = new HashMap<>();
+        inkwell = false;
     }
 
     public String getPlayerName() {
         return playerName;
-    }
-
-    public List<Integer> getLeaderCards() {
-        return leaderCards;
-    }
-
-    public void setLeaderCards(List<Integer> leaderCards) {
-        this.leaderCards = leaderCards;
-    }
-
-    public Stack<Integer>[] getDevelopmentCardSlots() {
-        return developmentCardSlots;
     }
 
     public void setInkwell(boolean inkwell) {
@@ -66,49 +56,64 @@ public class LightPersonalBoard {
         return baseProduction;
     }
 
-    public List<Production> getLeaderProductions() {
-        return leaderProductions;
+    public Stack<Integer>[] getDevelopmentCardSlots() {
+        return developmentCardSlots;
     }
-
-    public void addLeaderProduction(Production newLeaderProduction) {
-        leaderProductions.add(newLeaderProduction);
+    public void setDevCardSlot(int id, int slot){
+        developmentCardSlots[slot].push(id);
     }
-
-    public void removeLeaderProduction(Production leaderProduction){leaderProductions.remove(leaderProduction);}
 
     public List<Map.Entry<ResType, Integer>> getDepots() {
         return depots;
     }
-
     public void setDepots(List<Map.Entry<ResType, Integer>> depots) {
         this.depots = depots;
-    }
-
-    public List<Map.Entry<ResType, Integer>> getLeaderDepots() {
-        return leaderDepots;
-    }
-
-    public void setLeaderDepots(List<Map.Entry<ResType, Integer>> leaderDepots) {
-        this.leaderDepots = leaderDepots;
     }
 
     public Map<ResType, Integer> getStrongbox() {
         return strongbox;
     }
-
     public void setStrongbox(Map<ResType, Integer> strongbox) {
         this.strongbox = strongbox;
     }
 
-    public void setDevCardSlot(int id, int slot){
+    public List<Integer> getLeaderCards() {
+        return leaderCards;
+    }
+    public void setLeaderCards(List<Integer> leaderCards) {
+        this.leaderCards = leaderCards;
+    }
+    public void discardLeaderCard(int id){
+        leaderCards.remove(Integer.valueOf(id));
+    }
+
+    public List<Production> getLeaderProductions() {
+        return leaderProductions;
+    }
+    public void addLeaderProduction(Production newLeaderProduction) {
+        leaderProductions.add(newLeaderProduction);
+    }
+    public void removeLeaderProduction(Production leaderProduction){leaderProductions.remove(leaderProduction);}
+
+    public List<Map.Entry<ResType, Integer>> getLeaderDepots() {
+        return leaderDepots;
+    }
+    public void setLeaderDepots(List<Map.Entry<ResType, Integer>> leaderDepots) {
+        this.leaderDepots = leaderDepots;
+    }
+    public void addLeaderDepot(Depot depot) {
+        leaderDepots.add(new AbstractMap.SimpleEntry<>(depot.getDepotResources(),0));
+    }
+    public void removeLeaderDepot(Depot depot) {
+        leaderDepots.removeIf(me -> me.getKey() == depot.getDepotResources());
+    }
+
     public void addLeaderConvert(ResType newLeaderConvert){
         leaderConverts.add(newLeaderConvert);
     }
-
     public void removeLeaderConvert(ResType leaderConvert){
         leaderConverts.remove(leaderConvert);
     }
-
     public List<ResType> getLeaderConverts() {
         return leaderConverts;
     }
@@ -116,28 +121,10 @@ public class LightPersonalBoard {
     public void addLeaderDiscount(Map<ResType,Integer> newLeaderDiscount){
         leaderDiscounts.add(newLeaderDiscount);
     }
-
     public void removeLeaderDiscount(Map<ResType,Integer> leaderDiscount){
         leaderDiscounts.remove(leaderDiscount);
     }
-
     public List<Map<ResType, Integer>> getLeaderDiscounts() {
         return leaderDiscounts;
-    }
-
-    public void setDevCardSlot(int id, int slot){
-        developmentCardSlots[slot].push(id);
-    }
-
-    public void discardLeaderCard(int id){
-        leaderCards.remove(Integer.valueOf(id));
-    }
-
-    public void addLeaderDepot(Depot depot) {
-        //TODO
-    }
-
-    public void removeLeaderDepot(Depot depot) {
-        //TODO
     }
 }
