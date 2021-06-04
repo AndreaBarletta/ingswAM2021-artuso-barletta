68130a68b4b7cd3b403c1026fc54ea0ddc87b743
Matteo Artuso <10610150@polimi.it>
2021-06-04 03:46:48 +0200

    remove unused imports

--- src/main/java/it/polimi/ingsw/view/gui/GuiController.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiController.java
@@ -1,12 +1,9 @@
 package it.polimi.ingsw.view.gui;
 
-import it.polimi.ingsw.view.View;
-import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class GuiController {
-
     public Button b1;
 
     @FXML
