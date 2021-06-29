8b29e9d35a1cc35c1dcb1321e0fb66ab660894b9
lubest99 <62891857+lubest99@users.noreply.github.com>
2021-06-29 23:43:54 +0200

    Completed leader cards choice scene.

--- src/main/java/it/polimi/ingsw/view/gui/GuiControllerLeaderCardChoice.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiControllerLeaderCardChoice.java
@@ -1,18 +1,26 @@
 package it.polimi.ingsw.view.gui;
 
+import it.polimi.ingsw.Message;
+import it.polimi.ingsw.MessageType;
 import it.polimi.ingsw.view.LightModel;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Text;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
-public class GuiControllerLeaderCardChoice {
+public class GuiControllerLeaderCardChoice<event> {
 
+    private String[] cardsIds = new String[4];
+    private boolean[] selected = new boolean[4];
     private PrintWriter out;
     @FXML
     private ImageView iv1;
@@ -22,12 +30,24 @@ public class GuiControllerLeaderCardChoice {
     private ImageView iv3;
     @FXML
     private ImageView iv4;
+    @FXML
+    private Text t1;
+    @FXML
+    private Text t2;
+    @FXML
+    private Text t3;
+    @FXML
+    private Text t4;
 
     public void setOutPrintWriter(PrintWriter out) {
         this.out = out;
     }
 
     public void showLeaderCards(String[] leaderCardsIds, LightModel lightModel) {
+        cardsIds[0] = leaderCardsIds[0];
+        cardsIds[1] = leaderCardsIds[1];
+        cardsIds[2] = leaderCardsIds[2];
+        cardsIds[3] = leaderCardsIds[3];
         String img1path=lightModel.getLeaderCardDeck()[Integer.parseInt(leaderCardsIds[0])].getCardFront();
         String img2path=lightModel.getLeaderCardDeck()[Integer.parseInt(leaderCardsIds[1])].getCardFront();
         String img3path=lightModel.getLeaderCardDeck()[Integer.parseInt(leaderCardsIds[2])].getCardFront();
@@ -41,4 +61,62 @@ public class GuiControllerLeaderCardChoice {
         iv3.setImage(lc3);
         iv4.setImage(lc4);
     }
+
+    public void pickLeaderCards(){
+        ArrayList leaderChosen = new ArrayList();
+        if(selected[0])
+            leaderChosen.add(cardsIds[0]);
+        if(selected[1])
+            leaderChosen.add(cardsIds[1]);
+        if(selected[2])
+            leaderChosen.add(cardsIds[2]);
+        if(selected[3])
+            leaderChosen.add(cardsIds[3]);
+        out.println(new Message(MessageType.CHOOSE_LEADER_CARDS, new String[]{leaderChosen.get(0).toString(), leaderChosen.get(1).toString()}));
+    }
+
+    public void selectCard1(){
+        if(selected[0] == true) {
+            selected[0] = false;
+            t1.setText("");
+        }
+        else{
+            selected[0] = true;
+            t1.setText("Selected");
+        }
+    }
+
+    public void selectCard2(){
+        if(selected[1] == true) {
+            selected[1] = false;
+            t2.setText("");
+        }
+        else{
+            selected[1] = true;
+            t2.setText("Selected");
+        }
+    }
+
+    public void selectCard3(){
+        if(selected[2] == true) {
+            selected[2] = false;
+            t3.setText("");
+        }
+        else{
+            selected[2] = true;
+            t3.setText("Selected");
+        }
+    }
+
+    public void selectCard4(){
+        if(selected[3] == true) {
+            selected[3] = false;
+            t4.setText("");
+        }
+        else{
+            selected[3] = true;
+            t4.setText("Selected");
+        }
+    }
+
 }
