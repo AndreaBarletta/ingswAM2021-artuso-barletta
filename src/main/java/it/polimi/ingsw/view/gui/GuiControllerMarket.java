2bf9c30289d23c3b04f9a84908cdd898078a980f
Luca-Arpaia <62891857+lubest99@users.noreply.github.com>
2021-07-01 10:50:24 +0200

    Changed ImageView[][] market to ImageView[][] marketTray.

--- src/main/java/it/polimi/ingsw/view/gui/GuiControllerMarket.java
+++ src/main/java/it/polimi/ingsw/view/gui/GuiControllerMarket.java
@@ -13,28 +13,28 @@ public class GuiControllerMarket {
     @FXML
     private ImageView leftover;
     @FXML
-    ImageView[][] market = new ImageView[3][4];
+    ImageView[][] marketTray = new ImageView[3][4];
 
     public void createMarketImageViews(){
-        market[0][0] = b11;
-        market[0][1] = b12;
-        market[0][2] = b13;
-        market[0][3] = b14;
-        market[1][0] = b21;
-        market[1][1] = b22;
-        market[1][2] = b23;
-        market[1][3] = b24;
-        market[2][0] = b31;
-        market[2][1] = b32;
-        market[2][2] = b33;
-        market[2][3] = b34;
+        marketTray[0][0] = b11;
+        marketTray[0][1] = b12;
+        marketTray[0][2] = b13;
+        marketTray[0][3] = b14;
+        marketTray[1][0] = b21;
+        marketTray[1][1] = b22;
+        marketTray[1][2] = b23;
+        marketTray[1][3] = b24;
+        marketTray[2][0] = b31;
+        marketTray[2][1] = b32;
+        marketTray[2][2] = b33;
+        marketTray[2][3] = b34;
     }
 
     public void update(LightModel lightModel){
         for(int i=0; i<3; i++){
             for(int j=0; j<4; j++){
                 Image murble = new Image(lightModel.getLightMarket().getMarketTray()[i][j].getPath());
-                market[i][j].setImage(murble);
+                marketTray[i][j].setImage(murble);
                 }
             }
         Image leftoverMurble = new Image(lightModel.getLightMarket().getLeftoverMarble().getPath());
